fb267e96426a6a8860ac4a22a1f078169ccaee8c
JapanPanda <japanxpanda@yahoo.com>
2021-03-21 23:53:07 -0700

    Added signup functionality

--- backend/api/routes/user/index.js
+++ backend/api/routes/user/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const route = express.Router();
+const authService = require('../../../services/authService');
 
 module.exports = (app) => {
   app.use('/user', route);
 
-  route.get('/', (req, res) => {
-    return res.json({ res: 'cool' }).status(200);
+  route.post('/signup', async (req, res) => {
+    const { user, error } = await authService.signup(req.body);
+
+    if (error) {
+      return res.status(400).json({ error: error, msg: 'Error' });
+    }
+
+    return res.json(`Successfully signed up ${user.username}.`);
   });
 };
---
+++ backend/services/authService.js
@@ -0,0 +1,99 @@
+const logger = require('../loaders/logger');
+
+const db = require('../loaders/database');
+
+const yup = require('yup');
+
+const argon = require('argon2');
+
+const signupSchema = yup.object().shape({
+  username: yup.string().required().min(3).max(16),
+  password: yup.string().required().min(6).max(254),
+  email: yup.string().required().email(),
+});
+
+const signup = async (user) => {
+  return signupSchema
+    .validate(user)
+    .then(async (validatedUser) => {
+      // check to see if username and email are unique
+      try {
+        let userbyUsername = await findUserbyUsername(validatedUser.username);
+
+        let userbyEmail = await findUserbyEmail(validatedUser.email);
+
+        if (userbyEmail && userbyUsername) {
+          return { user: null, error: 'Email and username already taken!' };
+        } else if (userbyEmail) {
+          return { user: null, error: `Email already taken!` };
+        } else if (userbyUsername) {
+          return { user: null, error: 'Username already taken!' };
+        }
+      } catch (err) {
+        return { user: null, error: 'Something went horribly wrong...' };
+      }
+
+      const hashedPassword = await argon.hash(user.password);
+
+      // attempt to insert user into database
+      return db
+        .none(
+          `INSERT INTO account(username, email, password, liked, disliked, saved)
+                VALUES($1, $2, $3, $4, $5, $6);`,
+          [user.username, user.email, hashedPassword, [], [], []]
+        )
+        .then(() => {
+          logger.info(`Successfully added user ${user.username} to database.`);
+          return { user: user, error: null };
+        })
+        .catch((err) => {
+          logger.error(`Error inserting user: ${user.username}\n${err.stack}`);
+          return { user: null, error: err };
+        });
+    })
+    .catch((err) => {
+      logger.error(`Error signing up user.\n${err}`);
+
+      return { user: null, error: err };
+    });
+};
+
+const findUserbyUsername = async (username) => {
+  return db
+    .oneOrNone(
+      `SELECT username from account
+                  WHERE username = $1`,
+      [username]
+    )
+    .then((user) => {
+      return user;
+    })
+    .catch((err) => {
+      logger.error(
+        `Error querying for user through username ${username}.\n${err}`
+      );
+      throw new Error(`Error querying for user through username ${username}`);
+    });
+};
+
+const findUserbyEmail = async (email) => {
+  return db
+    .oneOrNone(
+      `SELECT email from account
+                  WHERE email = $1`,
+      [email]
+    )
+    .then((user) => {
+      return user;
+    })
+    .catch((err) => {
+      logger.error(
+        `Error querying for user through username ${username}.\n${err}`
+      );
+      throw new Error(`Error querying for user through username ${username}`);
+    });
+};
+
+module.exports = {
+  signup: signup,
+};
